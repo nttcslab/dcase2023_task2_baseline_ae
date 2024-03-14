02d19a19fbc433b677395c984ec26ce5eea3617e
YuriMusashijima <yuri.musashijima.uh@hco.ntt.co.jp>
2024-03-14 12:01:10 +0900

    2024で学習・推論を行えるようにした
    動作確認はダミーデータで実施した

--- data_download_2024dev.sh
+++ data_download_2024dev.sh
@@ -2,7 +2,7 @@ mkdir -p "data/dcase2024t2/dev_data/raw"
 
 # download dev data
 cd "data/dcase2024t2/dev_data/raw"
-for machine_type in ToyCar ToyTrain; do
+for machine_type in bearing fan gearbox slider ToyCar ToyTrain valve; do
 wget "/files/dev_${machine_type}.zip"
 unzip "dev_${machine_type}.zip"
 done
