8dfac4b74a7155111388a34f0d7bf44f5c470c51
YuriMusashijima <yuri.musashijima.uh@hco.ntt.co.jp>
2024-04-01 18:56:47 +0900

    ダウンロードURLを更新した

--- data_download_2024dev.sh
+++ data_download_2024dev.sh
@@ -3,6 +3,6 @@ mkdir -p "data/dcase2024t2/dev_data/raw"
 # download dev data
 cd "data/dcase2024t2/dev_data/raw"
 for machine_type in bearing fan gearbox slider ToyCar ToyTrain valve; do
-wget "https://zenodo.org/record/10850880/files/dev_${machine_type}.zip"
+wget "https://zenodo.org/record/10902294/files/dev_${machine_type}.zip"
 unzip "dev_${machine_type}.zip"
 done
