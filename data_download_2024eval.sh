04ed90dadf15d7e085e261a86f5fb0f6cac7b3e3
YuriMusashijima <89237813+YuriMusashijima@users.noreply.github.com>
2024-05-31 19:40:03 +0900

    Supported Evaluation Dataset (#13)

---
+++ data_download_2024eval.sh
@@ -0,0 +1,11 @@
+parent_dir=data
+ROOT_DIR=$(cd $(dirname $0); pwd)/
+mkdir -p "${ROOT_DIR}/${parent_dir}/dcase2024t2/eval_data/raw"
+
+# download eval data
+cd "${parent_dir}/dcase2024t2/eval_data/raw"
+for machine_type in bandsaw grinder shaker ToyDrone ToyNscale ToyTank Vacuum; do
+wget "https://zenodo.org/records/11363076/files/eval_data_${machine_type}_test.zip"
+unzip "eval_data_${machine_type}_test.zip"
+done
+
