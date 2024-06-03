55dbfc96855b3b0eedf565a1f6e690ac8eba783d
YuriMusashijima <89237813+YuriMusashijima@users.noreply.github.com>
2024-06-03 12:43:56 +0900

    Fix downlaod script for evaluation dataset (#15)
    * Update README.md
    
    * Update README_legacy.md
    
    * supported additional training dataset
    
    * Update README.md
    
    * fix additional training data
    
    * supported evaluation dataset
    
    * fix download script
    
    2024t2 eval data url fixed
    
    ---------
    
    Co-authored-by: Noboru Harada <64912994+noboru2000@users.noreply.github.com>

--- data_download_2024eval.sh
+++ data_download_2024eval.sh
@@ -4,7 +4,17 @@ mkdir -p "${ROOT_DIR}/${parent_dir}/dcase2024t2/eval_data/raw"
 
 # download eval data
 cd "${parent_dir}/dcase2024t2/eval_data/raw"
-for machine_type in bandsaw grinder shaker ToyDrone ToyNscale ToyTank Vacuum; do
+for machine_type in \
+    3DPrinter \
+    AirCompressor \
+    Scanner \
+    ToyCircuit \
+    HoveringDrone \
+    HairDryer \
+    ToothBrush \
+    RoboticArm \
+    BrushlessMotor \
+; do
 wget "https://zenodo.org/records/11363076/files/eval_data_${machine_type}_test.zip"
 unzip "eval_data_${machine_type}_test.zip"
 done
